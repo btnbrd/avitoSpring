73942b9d0433c59faceb6a07e34bbd8d36e88362
filinan <filin.aa@phystech.edu>
2025-04-11 14:25:28 +0300

    added migrations

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE IF NOT EXISTS users (
+                       id SERIAL PRIMARY KEY,
+                       name VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pvz (
+                                   id SERIAL PRIMARY KEY,
+                                   city VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE IF NOT EXISTS goods_receipt (
+                                             id SERIAL PRIMARY KEY,
+                                             pvz_id INTEGER REFERENCES pvz(id),
+    receipt_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) CHECK (status IN ('in_progress', 'close')) NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS goods (
+                                     id SERIAL PRIMARY KEY,
+                                     receipt_id INTEGER REFERENCES goods_receipt(id),
+    type VARCHAR(50) CHECK (type IN ('electronics', 'clothes', 'shoes')) NOT NULL,
+    received_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
