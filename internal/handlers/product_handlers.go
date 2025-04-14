2ad599a1121f6337d40d376c2ce83f3a90b444ec
filinan <filin.aa@phystech.edu>
2025-04-14 12:37:34 +0300

    product api logic

---
+++ internal/handlers/product_handlers.go
@@ -0,0 +1,79 @@
+package handlers
+
+import (
+	"avitoSpring/internal/models"
+	"avitoSpring/internal/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type ProductHandler struct {
+	productService *services.ProductService
+	authHandler    *AuthHandler
+}
+
+func NewProductHandler(productService *services.ProductService, authHandler *AuthHandler) *ProductHandler {
+	return &ProductHandler{
+		productService: productService,
+		authHandler:    authHandler,
+	}
+}
+
+func (h *ProductHandler) ProductHandler(c *gin.Context) {
+	role, _ := c.Get("role")
+	if role != models.RoleEmployee {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Only employees can create products"})
+		return
+	}
+
+	var req struct {
+		Type  models.ProductType `json:"type" binding:"required"`
+		PVZID string             `json:"pvzId" binding:"required,uuid"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	product := &models.Product{
+		Type: req.Type,
+	}
+
+	productID, err := h.productService.CreateProduct(product, req.PVZID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"id": productID,
+	})
+}
+
+func (h *ProductHandler) DeleteLastProductHandler(c *gin.Context) {
+	pvzID := c.Param("pvzId")
+	if pvzID == "" {
+		c.JSON(http.StatusBadRequest, models.Error{
+			Message: "pvzId is required",
+		})
+		return
+	}
+
+	role, _ := c.Get("role")
+	if role != models.RoleEmployee {
+		c.JSON(http.StatusForbidden, models.Error{
+			Message: "Only employees can delete products",
+		})
+		return
+	}
+
+	err := h.productService.DeleteLastProduct(pvzID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.Error{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	c.Status(http.StatusOK)
+}
--- internal/storage/product.go
+++ internal/storage/product.go
@@ -4,4 +4,5 @@ import "avitoSpring/internal/models"
 
 type ProductStorage interface {
 	CreateProduct(product *models.Product) (string, error)
+	DeleteLastProduct(pvzID string) error
 }
