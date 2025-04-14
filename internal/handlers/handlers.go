2a956eec4f6bb562bfd616a8dbad86ba619cefda
filinan <filin.aa@phystech.edu>
2025-04-14 19:46:44 +0300

    logic for authentication in special files

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,22 +1,28 @@
 package handlers
 
-import "avitoSpring/internal/services"
+import (
+	"avitoSpring/internal/middleware"
+	"avitoSpring/internal/server"
+	"avitoSpring/internal/services"
+)
 
-func RegisterHandlers(s *services.Server, authService *services.AuthService, pvzService *services.PVZService, receptionService *services.ReceptionService, productService *services.ProductService) {
+func RegisterHandlers(s *server.Server, authService *services.AuthService,
+	pvzService *services.PVZService, receptionService *services.ReceptionService,
+	productService *services.ProductService, jwtService *services.JWTService) {
 
 	authHandler := NewAuthHandler(authService)
 	pvzHandler := NewPVZHandler(pvzService, authHandler)
 	receptionHandler := NewReceptionHandler(receptionService, authHandler)
 	productHandler := NewProductHandler(productService, authHandler)
-
+	authenticator := middleware.NewAuthenticator(jwtService)
 	s.POST("/dummyLogin", authHandler.DummyLoginHandler)
 	s.POST("/register", authHandler.RegisterHandler)
 	s.POST("/login", authHandler.LoginHandler)
-	s.POST("/pvz", authHandler.AuthMiddleware(), pvzHandler.PvzHandler)
-	s.GET("/pvz", authHandler.AuthMiddleware(), pvzHandler.PvzGetHandler)
-	s.POST("/receptions", authHandler.AuthMiddleware(), receptionHandler.ReceptionHandler)
-	s.POST("/products", authHandler.AuthMiddleware(), productHandler.ProductHandler)
+	s.POST("/pvz", authenticator.AuthMiddleware(), pvzHandler.PvzHandler)
+	s.GET("/pvz", authenticator.AuthMiddleware(), pvzHandler.PvzGetHandler)
+	s.POST("/receptions", authenticator.AuthMiddleware(), receptionHandler.ReceptionHandler)
+	s.POST("/products", authenticator.AuthMiddleware(), productHandler.ProductHandler)
 
-	s.POST("/pvz/:pvzId/close_last_reception", authHandler.AuthMiddleware(), receptionHandler.CloseLastReceptionHandler)
-	s.POST("/pvz/:pvzId/delete_last_product", authHandler.AuthMiddleware(), productHandler.DeleteLastProductHandler)
+	s.POST("/pvz/:pvzId/close_last_reception", authenticator.AuthMiddleware(), receptionHandler.CloseLastReceptionHandler)
+	s.POST("/pvz/:pvzId/delete_last_product", authenticator.AuthMiddleware(), productHandler.DeleteLastProductHandler)
 }
